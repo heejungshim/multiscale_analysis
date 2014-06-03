adb1446ca27b32ad8cd0baee7d5909975e7577ce
heejungshim <hjshim@gmail.com>
2014-06-02 22:32:03 -0500

    add codes to collect pvalues from analyses

---
+++ analysis/simulation/sample_size/simulation_footprint/code/get.pval.ms.R
@@ -0,0 +1,84 @@
+
+
+numSites = 200
+
+
+path.alt = "/mnt/lustre/home/shim/multiscale_analysis/analysis/simulation/sample_size/simulation_footprint/alt/multiscale/"
+path.null = "/mnt/lustre/home/shim/multiscale_analysis/analysis/simulation/sample_size/simulation_footprint/null/multiscale/"
+
+
+case.name = c("fullread.70ind", "fullread.30ind", "fullread.10ind")
+
+
+for(cc in 1:length(case.name)){
+
+    pval_list = rep(NA, numSites)
+    done_res = rep(NA, numSites)
+
+    IX = 1
+
+    for(i in 1:numSites){
+   
+        num_path  = paste0(path.alt, case.name[cc], ".output/pval.", i, ".out")
+        if(file.exists(num_path)== FALSE){		
+            done_res[IX] = FALSE
+        }else{
+            if(file.info(num_path)$size == 0){
+                done_res[IX] = FALSE
+            }else{
+                dat = scan(num_path, what="")
+                if(!is.na(dat[4])){
+                    pval_list[IX] = as.numeric(dat[4])
+                    done_res[IX] = TRUE
+                }else{
+                    done_res[IX] = FALSE
+                }
+            }
+        }
+        IX = IX + 1
+    }
+
+    save("pval_list", "done_res", file =paste0(path.alt, "sum/pval.", case.name[cc], ".Robj"))
+
+
+}
+
+
+
+
+for(cc in 1:length(case.name)){
+
+    pval_list = rep(NA, numSites)
+    done_res = rep(NA, numSites)
+
+    IX = 1
+
+    for(i in 1:numSites){
+   
+        num_path  = paste0(path.null, case.name[cc], ".output/pval.", i, ".out")
+        if(file.exists(num_path)== FALSE){		
+            done_res[IX] = FALSE
+        }else{
+            if(file.info(num_path)$size == 0){
+                done_res[IX] = FALSE
+            }else{
+                dat = scan(num_path, what="")
+                if(!is.na(dat[4])){
+                    pval_list[IX] = as.numeric(dat[4])
+                    done_res[IX] = TRUE
+                }else{
+                    done_res[IX] = FALSE
+                }
+            }
+        }
+        IX = IX + 1
+    }
+
+    save("pval_list", "done_res", file =paste0(path.null, "sum/pval.", case.name[cc], ".Robj"))
+
+    
+}
+
+
+
+
---
+++ analysis/simulation/sample_size/simulation_footprint/code/get.pval.wave.R
@@ -0,0 +1,81 @@
+
+
+numSites = 200
+
+
+path.alt = "/mnt/lustre/home/shim/multiscale_analysis/analysis/simulation/sample_size/simulation_footprint/alt/wave/"
+path.null = "/mnt/lustre/home/shim/multiscale_analysis/analysis/simulation/sample_size/simulation_footprint/null/wave/"
+
+
+case.name = c("fullread.70ind", "fullread.30ind", "fullread.10ind")
+
+
+for(cc in 1:length(case.name)){
+
+    pval_list = rep(NA, numSites)
+    done_res = rep(NA, numSites)
+
+    IX = 1
+
+    num_dir_path = paste0(path.alt, "output/", case.name[cc], ".")
+
+    for(i in 1:numSites){
+   
+        num_path  = paste0(num_dir_path, i, ".fph.pval.txt")
+        if(file.exists(num_path)== FALSE){		
+            done_res[IX] = FALSE
+        }else{
+            if(file.info(num_path)$size == 0){
+		done_res[IX] = FALSE
+            }else{			
+		dat = read.table(num_path, as.is = TRUE)
+		if(!is.na(dat[4,])){
+                    pval_list[IX] = dat[4,]
+                    done_res[IX] = TRUE
+                }else{
+                    done_res[IX] = FALSE
+                }
+            }
+        }
+        IX = IX + 1
+    }
+
+    save("pval_list", "done_res", file =paste0(path.alt, "sum/pval.", case.name[cc], ".Robj"))
+
+}
+
+
+
+for(cc in 1:length(case.name)){
+
+    pval_list = rep(NA, numSites)
+    done_res = rep(NA, numSites)
+
+    IX = 1
+
+    num_dir_path = paste0(path.null, "output/", case.name[cc], ".")
+
+    for(i in 1:numSites){
+   
+        num_path  = paste0(num_dir_path, i, ".fph.pval.txt")
+        if(file.exists(num_path)== FALSE){		
+            done_res[IX] = FALSE
+        }else{
+            if(file.info(num_path)$size == 0){
+		done_res[IX] = FALSE
+            }else{			
+		dat = read.table(num_path, as.is = TRUE)
+		if(!is.na(dat[4,])){
+                    pval_list[IX] = dat[4,]
+                    done_res[IX] = TRUE
+                }else{
+                    done_res[IX] = FALSE
+                }
+            }
+        }
+        IX = IX + 1
+    }
+
+    save("pval_list", "done_res", file =paste0(path.null, "sum/pval.", case.name[cc], ".Robj"))
+
+}
