785092ff4285eeb5580d80e3675ece8fa643d9b7
heejungshim <hjshim@gmail.com>
2014-06-04 15:26:23 -0500

    modify a function estBetaParams to handle 0 or 1

--- src/R/utils.R
+++ src/R/utils.R
@@ -4,12 +4,39 @@
 
 
 ## 'estBetaParams' takes mean and variance for beta distribution and returns alpha and beta parameters.  
-## I copied from http://stats.stackexchange.com/questions/12232/calculating-the-parameters-of-a-beta-distribution-using-the-mean-and-variance
-estBetaParams <- function(mu, var) {
+## I copied the original version from http://stats.stackexchange.com/questions/12232/calculating-the-parameters-of-a-beta-distribution-using-the-mean-and-variance
+## Then I modified to handle cases when somme elements of mu are 0 or 1 (then it's always 0 or 1 as I don't allow variation.)
+estBetaParams <- function(mu.orig, var) {
+
+    del.ix = ((mu.orig == 0) | (mu.orig == 1))
+    if(sum(del.ix) > 0){
+        mu = mu.orig[!del.ix]
+    }else{
+        mu = mu.orig
+    }
     alpha <- ((1 - mu) / var - 1 / mu) * mu ^ 2
     beta <- alpha * (1 / mu - 1)
-    return(params = list(alpha = alpha, beta = beta))
+
+    if(sum(del.ix) > 0){
+        alpha.orig = beta.orig = rep(NA, length(mu.orig))
+        alpha.orig[!del.ix] = alpha
+        beta.orig[!del.ix] = beta
+    }else{
+        alpha.orig = alpha
+        beta.orig = beta
+    }
+    return(params = list(alpha = alpha.orig, beta = beta.orig))
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
