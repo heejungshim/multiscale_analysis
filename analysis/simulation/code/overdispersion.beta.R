52b9f0c2c59cc17701e3e8269e12bdececfd9fe5
heejungshim <hjshim@gmail.com>
2014-06-04 14:06:37 -0500

    selection of over dispersion

---
+++ analysis/simulation/code/overdispersion.beta.R
@@ -0,0 +1,54 @@
+
+# function to compute alpha and beta from mean and varince of beta distribution
+# copyed from http://stats.stackexchange.com/questions/12232/calculating-the-parameters-of-a-beta-distribution-using-the-mean-and-variance
+estBetaParams <- function(mu, var) {
+     alpha <- ((1 - mu) / var - 1 / mu) * mu ^ 2
+     beta <- alpha * (1 / mu - 1)
+     return(params = list(alpha = alpha, beta = beta))
+}
+
+
+mu = 1/70
+var = 1/70/70
+
+alpha = estBetaParams(mu, var)$alpha
+beta = estBetaParams(mu, var)$beta
+
+sam1 = rbeta(10000, alpha, beta)
+
+
+mu = 1/70
+var = 1/70/70/10
+
+alpha = estBetaParams(mu, var)$alpha
+beta = estBetaParams(mu, var)$beta
+
+sam2 = rbeta(10000, alpha, beta)
+
+
+
+mu = 1/70
+var = 1/70/70/100
+
+alpha = estBetaParams(mu, var)$alpha
+beta = estBetaParams(mu, var)$beta
+
+sam3 = rbeta(10000, alpha, beta)
+
+
+## make a histrogam
+
+pdf("beta.samples.pdf")
+par(mfrow=c(3,1))
+hist(sam1, breaks =100, main = "mean: 1/70, var: 1/70/70")
+abline(v = 1/70, col="red")
+
+hist(sam2, breaks =100, main = "mean: 1/70, var: 1/70/70/10")
+abline(v = 1/70, col="red")
+
+hist(sam3, breaks =100, main = "mean: 1/70, var: 1/70/70/100")
+abline(v = 1/70, col="red")
+
+dev.off()
+
+
