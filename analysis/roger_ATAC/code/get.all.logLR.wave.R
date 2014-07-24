6adda5e7af5433de439e68ab671e1dc8a0586691
heejungshim <hjshim@gmail.com>
2014-07-23 23:17:18 -0500

    colect all logLR

---
+++ analysis/roger_ATAC/code/get.all.logLR.wave.R
@@ -0,0 +1,40 @@
+#!/usr/bin/env Rscript
+
+## Aim : This file contains Rscript to collect logLR from different chromosome 
+##
+## Copyright (C) 2014 Heejung Shim
+##
+## License: GPL3+
+
+
+
+wd.path='/mnt/lustre/home/shim/multiscale_analysis/analysis/roger_ATAC/run/wave/'
+siteSize=2048
+treatment='Copper'
+null=TRUE
+strand='plus'
+
+    
+## directory name 
+if(!null){
+    com.dir.name=paste0(treatment,".", siteSize, ".", strand, ".alt")
+}else{
+    com.dir.name=paste0(treatment,".", siteSize, ".", strand, ".null")
+}
+
+
+path = paste0(wd.path, com.dir.name, ".run/sum/", com.dir.name, ".")
+done_list = vector("list", 22)
+logLR_list = vector("list", 22)
+
+for(chr in 1:22){
+    load(paste0(path, chr, ".Robj"))
+    done_list[[chr]] = done
+    logLR_list[[chr]] = logLR
+}
+
+
+save("logLR_list", "done_list", file =paste0(path, "Robj"))
+
+
+
