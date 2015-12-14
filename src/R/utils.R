b0323912bf6f54da777bc61db92e2be3d93195b2
heejungshim <hjshim@gmail.com>
2015-12-13 19:59:44 -0600

    as default do not print message

--- src/R/utils.R
+++ src/R/utils.R
@@ -25,11 +25,13 @@ library(rhdf5)
 #  function to Get counts from hdf5
 #  written by Ester Pantaleo
 #**************************************************************************
-get.counts.h5 <- function(list_path, chr, locus.start, locus.end, list_name = NULL){
+get.counts.h5 <- function(list_path, chr, locus.start, locus.end, list_name = NULL, print_message=FALSE){
     M <-  NULL
     for (h5file in list_path){
-        print(paste0("Loading ", h5file))
-        print(paste0("h5read(", h5file, ",", chr, ", index=list(", locus.start, ":", locus.end, ")"))
+        if(print_message){
+          print(paste0("Loading ", h5file))
+          print(paste0("h5read(", h5file, ",", chr, ", index=list(", locus.start, ":", locus.end, ")"))
+        }
         M        <- rbind(M, h5read(h5file, chr, index=list(locus.start:locus.end)))
     }
     row.names(M) = list_name
